091798a8ae64fc02c9bd1e978ab4760b108dd4d1
avelikan23 <alexvelikan@yahoo.com>
2024-11-27 10:33:10 -0800

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -69,13 +69,13 @@ def metrics(zipped_list):
   fn = sum([1 if pair==[0,1] else 0 for pair in zipped_list])
 
   #now can compute precicision, recall, f1, accuracy. Watch for divide by 0.
-  precision = round((tp/(tp+fp) if (tp+fp) else 0),2)
-  recall = round((tp/(tp+fn) if (tp+fn) else 0),2)
-  f1 = round((2*((precision*recall)/(precision+recall)) if (precision+recall) else 0),2)
-  accuracy = round(((tp+tn)/(tp+tn+fp+fn) if (tp+tn+fp+fn) else 0),2)
+  precision = (tp/(tp+fp) if (tp+fp) else 0)
+  recall = (tp/(tp+fn) if (tp+fn) else 0)
+  f1 = (2*((precision*recall)/(precision+recall)) if (precision+recall) else 0)
+  accuracy = ((tp+tn)/(tp+tn+fp+fn) if (tp+tn+fp+fn) else 0)
 
   #now build dictionary with the 4 measures - round values to 2 places
-  predict= {'Precision': precision, 'Recall': recall, 'F1': f1, 'Accuracy': accuracy}
+  predict= {'Precision': round(precision,2) 'Recall': round(recall,2) 'F1': round(f1,2) 'Accuracy': round(accuracy,2)}
 
   #finally, return the dictionary
   return predict
